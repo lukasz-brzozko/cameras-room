f0204bbe7a95fd6462c11b476bc46cd86facebc6
Łukasz Brzózko <lukasz9099@gmail.com>
2024-09-23 22:00:19 +0200

    feat: hide fake stream

--- components/ui/camera.tsx
+++ components/ui/camera.tsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useRef } from "react";
 
-function Camera({ stream }: { stream: MediaStream }) {
+function Camera({ stream }: { stream?: MediaStream }) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
+  const [{ enabled }] = stream?.getVideoTracks() ?? [{}];
+  const showVideo = enabled !== false;
+
   useEffect(() => {
-    if (videoRef.current) videoRef.current.srcObject = stream;
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
   }, [stream]);
 
   return (
-    <video className="w-full md:w-1/2" ref={videoRef} autoPlay muted>
-      Znacznik video nie jest wspierany przez przeglądarkę.
-    </video>
+    <>
+      {showVideo && (
+        <video className="w-full md:w-1/2" ref={videoRef} autoPlay muted>
+          Znacznik video nie jest wspierany przez przeglądarkę.
+        </video>
+      )}
+    </>
   );
 }
 
