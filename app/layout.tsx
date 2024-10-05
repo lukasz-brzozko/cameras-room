a90a2997cf0d3d7458be8ac226872ae85d71241f
Łukasz Brzózko <lukasz9099@gmail.com>
2024-10-05 18:27:46 +0200

    feat: dark mode adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} m-4 bg-slate-100 antialiased dark:bg-neutral-900 lg:mx-8`}
+        className={`${geistSans.variable} ${geistMono.variable} m-4 bg-slate-100 antialiased dark:bg-neutral-900 dark:text-primary-foreground lg:mx-8`}
       >
         {children}
       </body>
